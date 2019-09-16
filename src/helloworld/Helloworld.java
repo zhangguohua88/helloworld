bcdc8512d5706bc0620752e8c1d4588bafa88866
zhangguohua88 <55342198+zhangguohua88@users.noreply.github.com>
2019-09-16 17:27:11 +0900

    Update Helloworld.java

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -2,7 +2,8 @@ package helloworld;
 
 public class Helloworld {
 	public static void main(String[] args) {
-        System.out.println("你是我的最大的宝贝");
+        System.out.println("脛茫脢脟脦脪碌脛脳卯麓贸碌脛卤娄卤麓");
+		//浜戠淇敼
     }
 }
 	
