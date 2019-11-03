1f3587cf74080af63a1313c45102df02053c6a68
peter <you@example.com>
2019-11-03 10:03:08 +0900

    this is good (print)

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -1,22 +1,29 @@
 package helloworld;
 
 public class Helloworld {
+	
+	private String student;
+	
+	private String name;
+	
+	private String age;
 	public static void main(String[] args) {
 
 
-        System.out.println("你是我的最大的宝贝");
+        System.out.println("雿����憭抒����");
 
-        System.out.println("中国人口世界最大");
-		//本地备注
+        System.out.println("銝剖鈭箏銝��憭�");
+		//��憭釣
         System.out.print("this is good");
 
 
-        System.out.println("浣犳槸鎴戠殑鏈�澶х殑瀹濊礉");
+        System.out.println("瘚��瑽賊����蕭瞉剽��寞���");
 
-        System.out.println("涓浗浜哄彛涓栫晫鏈�澶�");
-		//鏈湴澶囨敞
-	//杩滅▼澶囨
+        System.out.println("瘨���������蕭瞉塚蕭");
+		//���皝湔噤����
+	//�皛瞉嗅
 	System.out.println("this is good thing to do");	
+	System.out.println("hello good");	
     }
 }
 	
