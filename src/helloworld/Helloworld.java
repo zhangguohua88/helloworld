1058dab67817ce91d137589af9f24db7eb01c605
peter <you@example.com>
2019-11-03 10:16:03 +0900

    this is good

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -10,20 +10,13 @@ public class Helloworld {
 	public static void main(String[] args) {
 
 
-        System.out.println("雿����憭抒����");
-
-        System.out.println("銝剖鈭箏銝��憭�");
-		//��憭釣
-        System.out.print("this is good");
-
-
-        System.out.println("瘚��瑽賊����蕭瞉剽��寞���");
-
-        System.out.println("瘨���������蕭瞉塚蕭");
-		//���皝湔噤����
-	//�皛瞉嗅
+        
 	System.out.println("this is good thing to do");	
 	System.out.println("hello good");	
+	System.out.println("this is one good ");
+	System.out.println("wo are good people ");
+	
+	
     }
 }
 	
