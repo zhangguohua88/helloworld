41180e33dcaf4433da58706de1c2255e6e2c4747
peter <you@example.com>
2019-11-03 10:29:45 +0900

    print one

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -4,19 +4,12 @@ public class Helloworld {
 	public static void main(String[] args) {
 
 
-        System.out.println("你是我的最大的宝贝");
-
-        System.out.println("中国人口世界最大");
-		//本地备注
-        System.out.print("this is good");
-
-
-        System.out.println("浣犳槸鎴戠殑鏈�澶х殑瀹濊礉");
-
-        System.out.println("涓浗浜哄彛涓栫晫鏈�澶�");
-		//鏈湴澶囨敞
-	//杩滅▼澶囨
-	System.out.println("this is good thing to do");	
+       
+	System.out.println("this is good thing to do");
+	   
+		System.out.println("wo are a good peopel");
+	
+	
     }
 }
 	
